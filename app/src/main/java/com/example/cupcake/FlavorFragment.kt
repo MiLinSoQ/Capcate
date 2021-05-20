d700f629bfd979c3cb76cc68db7a5492b9b65fda
MiLinSoQ <bluejacky258@gmail.com>
2021-05-20 11:02:03 +0800

    Practice android fragment in kotlin

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -62,6 +61,12 @@ class FlavorFragment : Fragment() {
     fun goToNextScreen() {
         // Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
         findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
+    }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+
+        findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
     }
 
     /**
